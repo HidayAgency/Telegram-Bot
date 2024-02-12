f078efe8e6acbf073bff2fbc27a5b313b7d2a9b8
HidayAgency <140704287+HidayAgency@users.noreply.github.com>
2024-02-12 13:20:55 +0100

    Create bot_tele.py
    Telegram bot

---
+++ bot_tele.py
@@ -0,0 +1,7 @@
+echo "# Telegram-Bot" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/HidayAgency/Telegram-Bot.git
+git push -u origin main
